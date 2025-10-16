a5bdbb5490a9fa46fd4ad435688956d4ad04162b
GOAT <lucasonyiego283@gmail.com>
2025-10-16 10:57:03 +0300

    defining the basic data types

--- main.cpp
+++ main.cpp
@@ -1,21 +1,16 @@
 
 #include<iostream>
+#include <sys/socket.h>
 
 
 int main(){
 
 
    std::cout << std::unitbuf;
+   std::cerr << std::unitbuf;
 
-   std::cout << "Hello";
+
 
    return 0;
 }
 
-
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/YOUNGGOAT34/http-server-cpp.git
-// git push -u origin main
---
+++ server.h
@@ -0,0 +1,16 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+//signed data types
+typedef char i8;
+typedef signed short int i16;
+typedef signed int i32;
+typedef signed long int i64;
+//unsigned data types
+typedef unsigned char u8;
+typedef unsigned short int u16;
+typedef unsigned int u32;
+typedef unsigned long int u64;
+
+
+#endif
